e166bd790959c66a95be84a0d2c1fb3fc6a457c7
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-22 17:13:53 -0300

    aula 05

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -52,7 +52,7 @@ export function NewHabitForm() {
       </label>
       <input
         type="text"
-        className="p-4 rounded-lg bg-zinc-800 text-white placeholder:text-zinc-400 mt-3"
+        className="p-4 rounded-lg bg-zinc-800 text-white placeholder:text-zinc-400 mt-3 focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-zinc-900"
         id="title"
         value={title}
         onChange={e => setTitle(e.target.value)}
@@ -70,10 +70,10 @@ export function NewHabitForm() {
             key={weekDay}
             checked={weekDays.includes(index)}
             onCheckedChange={() => handleToggleWeekDay(index)}
-            className='flex items-center gap-3 group'
+            className='flex items-center gap-3 group focus:outline-none'
           >
             <div
-              className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'
+              className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-700 group-focus:ring-offset-2 group-focus:ring-offset-background'
             >
               <Checkbox.Indicator>
                 <Check size={20} className='text-white' />
@@ -88,7 +88,10 @@ export function NewHabitForm() {
         })}
       </div>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500">
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center gap-3 font-semibold bg-green-600 justify-center hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
+      >
         <Check size={20} weight="bold" />
         Confirmar
       </button>
